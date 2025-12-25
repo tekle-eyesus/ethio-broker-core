e5debb1ca9cc4aab82503d54b9503ab869bad62f
tekle-eyesus <tekleeyesus21@gmail.com>
2025-12-25 06:40:43 -0700

    feat(auth): implement secure logout endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -102,9 +102,29 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-    // to be implemented after the middleware step, 
-    // but reserving the function name here.
-    return res.status(200).json(new ApiResponse(200, {}, "Logout placeholder"));
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined // Remove the refresh token from DB
+            }
+        },
+        {
+            new: true
+        }
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    };
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged out"));
 });
+
 
 export { registerUser, loginUser, logoutUser };
