436c3b94790959303cfb22823dde32bcb93a6ee9
tekle-eyesus <tekleeyesus21@gmail.com>
2026-01-05 08:58:07 -0700

    fix(app): set default CORS origin for development environment

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import connectDB from "./config/db.js";
 import { app } from "./app.js";
 
 dotenv.config({
-    path: "./.env",
+  path: "./.env",
 });
 
 connectDB()
-    .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-            console.log(`⚡ Server is running at port : ${process.env.PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log("MONGO db connection failed !!! ", err);
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      if (process.env.NODE_ENV !== "development") {
+        console.log(`⚡ Server is running at port : ${process.env.PORT}`);
+      }
     });
+  })
+  .catch((err) => {
+    // only for development logging
+    if (process.env.NODE_ENV === "development") {
+      console.log("MONGO db connection failed !!! ", err);
+    }
+  });
